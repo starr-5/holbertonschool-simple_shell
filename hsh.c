f4e2c1d518abcadd201d3ecf2a7ea91239f9d9cf
starr-5 <studying12567@gmail.com>
2026-02-19 15:23:04 +0000

    Task 1

--- hsh.c
+++ hsh.c
@@ -1,64 +1,77 @@
 #include "shell.h"
 
+void print_prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+	}
+}
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+		free(line);
+		if (isatty(STDIN_FILENO))
+			printf("\n");
+		exit(0);
+	}
+
+	if (line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	return line;
+}
+
+void execute_line(char *line)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(line);
+		exit(1);
+	}
+
+	if (pid == 0)
+	{
+		char *argv[] = {line, NULL};
+
+		execve(line, argv, environ);
+		perror("./shell");
+		exit(1);
+	}
+	else
+	{
+		wait(&status);
+	}
+}
+
 /**
  * main - simple shell
  * Return: 0
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-	int status;
+	char *line;
 
 	while (1)
 	{
-		/* Show prompt only if stdin is terminal */
-		if (isatty(STDIN_FILENO))
-		{
-			printf("#cisfun$ ");
-			fflush(stdout);
-		}
-
-		/* Read a line from stdin */
-		nread = getline(&line, &len, stdin);
-
-		/* Handle Ctrl+D or EOF */
-		if (nread == -1)
-		{
-			free(line);
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			exit(0);
-		}
-
-		/* Remove newline */
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
-
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			free(line);
-			exit(1);
-		}
-
-		if (pid == 0)
-		{
-			char *argv[] = {line, NULL};
-
-			execve(line, argv, environ);
-			perror("./shell"); /* Command not found */
-			exit(1);
-		}
-		else
-		{
-			wait(&status);
-		}
+		print_prompt();
+		line = read_line();
+		execute_line(line);
+		free(line);
 	}
 
-	free(line);
 	return (0);
 }
